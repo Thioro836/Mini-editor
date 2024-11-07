7c637af0d4970314827af6a5ea1943b82a1771a6
fbah <fatoumata-thioro.bah@etudiant.univ-rennes.fr>
2024-11-07 23:04:07 +0100

    modification des test de recorder

--- src/fr/istic/aco/editor/Test/ConcreteCommandTest.java
+++ src/fr/istic/aco/editor/Test/ConcreteCommandTest.java
@@ -1,14 +1,18 @@
 package fr.istic.aco.editor.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 
 import fr.istic.aco.editor.ClassImpl.EngineImpl;
 import fr.istic.aco.editor.ClassImpl.Invoker;
 import fr.istic.aco.editor.ClassImpl.RecorderImpl;
+import fr.istic.aco.editor.CommandOriginator.InsertCommand;
 import fr.istic.aco.editor.ConcreteCommandd.ReplayCommand;
 import fr.istic.aco.editor.ConcreteCommandd.StartCommand;
 import fr.istic.aco.editor.Interface.Command;
+import fr.istic.aco.editor.Interface.CommandOriginator;
 import fr.istic.aco.editor.Interface.Engine;
 import fr.istic.aco.editor.Interface.Recorder;
 import fr.istic.aco.editor.Interface.Selection;
@@ -19,7 +23,7 @@ public class ConcreteCommandTest {
     private Invoker invoker; // Instance d'Invoker
     private Selection selection;
     private Recorder recorder;
-    private Command command;
+    private CommandOriginator command;
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
         engine = new EngineImpl();
@@ -34,6 +38,13 @@ public class ConcreteCommandTest {
 
     @Test
     void startCommandTest(){
+        command=new InsertCommand(engine, invoker, recorder);
+        invoker.playCommandConcrete("start");
+        invoker.setTextToInsert("abcd");
+        invoker.playCommand("insert");
+        assertEquals("abcd", invoker.getTextToInsert());
+        recorder.save(command);
+        assertTrue(recorder.getList()>0);
      
 
     }
--- src/fr/istic/aco/editor/Test/RecorderTest.java
+++ src/fr/istic/aco/editor/Test/RecorderTest.java
@@ -35,43 +35,78 @@ public class RecorderTest {
     }
     @Test
     void start(){ 
-        cOriginator=new SelectionCommand(selection,invoker, recorder); 
+        cOriginator=new InsertCommand(engine,invoker, recorder); 
         recorder.start();
+        assertEquals("", engine.getBufferContents());
+        engine.insert("abcde");
         recorder.save(cOriginator);
+        assertEquals("abcde", engine.getBufferContents());
         assertTrue(recorder.getList()>0) ;
        
 
     }
     @Test
     void stop(){
+        //créer une instance de originatorcommand
         cOriginator=new SelectionCommand(selection,invoker, recorder); 
+        //commencer un enregistrement
         recorder.start();
+        //insérer du texte
+        engine.insert("abcde");
+        //faire une sélection
+        selection.setBeginIndex(1);
+        selection.setEndIndex(2);
+        //copier le texte 
+        engine.copySelectedText();
+        //sauvegarder l'enregistrement
         recorder.save(cOriginator);
+        //verifier le contenu du clipboard
+        assertEquals("b", engine.getClipboardContents());
+        //verifier que le recorder a bien ajouter la commande dans la liste
         assertEquals(1, recorder.getList());
+        //arreter l'enregistrement
         recorder.stop();
+        //faire un save de nouveau pour s'assurer que l'enregistrement ne sera pas pris en compte
         recorder.save(cOriginator);
         assertEquals(1, recorder.getList());
         
     }
     @Test
     void save(){
+        //demander à adrien si a chaque execution de commande je dois appeler start et stop ou pas
        cOriginator=new InsertCommand(engine,invoker, recorder);
-       recorder.start();    
+       recorder.start(); 
+       engine.insert("abcde");
        recorder.save(cOriginator);
+        assertEquals("abcde", engine.getBufferContents());
+       engine.insert("hello");  
        recorder.save(cOriginator);
        recorder.stop();
+       assertEquals("abcdehello", engine.getBufferContents());
       assertEquals(2, recorder.getList());
+      
     }
     @Test 
     void replay(){
         cOriginator=new InsertCommand(engine,invoker, recorder);
-        invoker.setTextToInsert("hello");
-        invoker.playCommand("insert");
-       
         recorder.start();
+        engine.insert("hello");
         recorder.save(cOriginator);
+        assertEquals("hello", engine.getBufferContents());
         recorder.stop();    
         recorder.replay();
-        assertEquals("hello", invoker.getTextToInsert());
+        assertEquals("hello", engine.getBufferContents());
+       
     }
+
+    @Test
+    @DisplayName("tester le cas où on fait un repaly sans faire de save")
+    void replayWithoutSave() {
+    recorder.start();
+    recorder.stop();
+    
+    recorder.replay();  
+    assertEquals("", engine.getBufferContents());  // Le tampon devrait être vide
+}
+
 }
